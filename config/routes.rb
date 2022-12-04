6a6fd06f347940f0c48121fd01b4d4f3af6b6616
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2022-12-04 14:22:30 +0000

    ouyoukadai3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
+  get 'favorites/create'
+  get 'favorites/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
@@ -8,6 +12,9 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
- 
+  resources :books do
+    resource :favorites , only: [:create , :destroy]
+    resources:book_comments,only:[:create,:destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
 end
