2e0d5ceb1fce4267312cafa7ba9b747e09d56475
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2022-12-06 09:22:43 +0000

    フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,42 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
   has_many :book_comments, dependent: :destroy
+  
 
+  # has_many :active_relationships, class_name:  "Relationship",foreign_key: "follower_id",dependent: :destroy
+  # has_many :passive_relationships, class_name:  "Relationship",foreign_key: "followed_id",dependent: :destroy
+  
+  # has_many :following, through: :active_relationships, source: :followed
+  # has_many :followers, through: :passive_relationships, source: :follower
+  
+  # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'book_comments/create'
   get 'book_comments/destroy'
   get 'favorites/create'
@@ -15,6 +18,11 @@ Rails.application.routes.draw do
   resources :books do
     resource :favorites , only: [:create , :destroy]
     resources:book_comments,only:[:create,:destroy]
+  end
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
 end
